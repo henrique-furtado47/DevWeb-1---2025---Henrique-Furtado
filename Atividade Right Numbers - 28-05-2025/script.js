c40c84858aed103dedbc7be73247b297c0cf45ca
Henrique <henrique.furtadoifc@gmail.com>
2025-05-29 14:12:35 -0300

    adição de áudios

--- Atividade Right Numbers - 28-05-2025/script.js
+++ Atividade Right Numbers - 28-05-2025/script.js
@@ -10,9 +10,19 @@ const mensagem = document.getElementById("variavel");
 const botoes = document.querySelectorAll(".numero");
 const botao_reiniciar = document.getElementById("restart");
 
+const somHover = new Audio("src/audio/hover.wav");
+const somAcerto = new Audio("src/audio/right.wav");
+const somErro = new Audio("src/audio/wrong.mp3");
+const somVitoria = new Audio("src/audio/victory.mp3");
+const somDerrota = new Audio("src/audio/gameover.wav");
+const somRestart = new Audio("src/audio/restart.mp3");
+
 console.log(tempoRestante);
 
 botoes.forEach((botao) => {
+  botao.addEventListener("mouseenter", () => {
+    somHover.cloneNode().play(); // toca hover
+  });
   botao.addEventListener("click", () => {
     if (tempoRestante === 30) {
       inciarCronometro();
@@ -30,15 +40,18 @@ botao_reiniciar.addEventListener("click", reiniciarJogo);
 function verificarTentativa(numero) {
   if (acertos < 3 && tempoRestante > 0) {
     if (numero === numeroSorteado) {
+      somAcerto.cloneNode().play();
       mensagem.innerText = "Você acertou o número!";
       acertos++;
       if (acertos >= 3) {
+        somVitoria.cloneNode().play();
         mensagem.innerText = "Parabéns, você venceu!";
       } else {
         numeroSorteado = Math.floor(Math.random() * 10);
         console.log(numeroSorteado);
       }
     } else {
+      somErro.cloneNode().play();
       if (numero > numeroSorteado) {
         mensagem.innerText = "O numéro é menor";
         velocidade -= 150;
@@ -61,6 +74,7 @@ function inciarCronometro() {
       console.log(velocidade);
     } else {
       if (acertos < 3) {
+        somDerrota.play();
         mensagem.innerText = "Você perdeu";
       } else {
         tempoRestante = tempoRestante;
@@ -87,6 +101,7 @@ function atualizarTempo() {
 }
 
 function reiniciarJogo() {
+  somRestart.play();
   clearInterval(cronometro);
   acertos = 0;
   numeroSorteado = Math.floor(Math.random() * 10);
