2dec6ba4d704ae0e9d0b1855f64b586f1e5212ea
henrique-furtado47 <henrique.furtadoifc@gmail.com>
2025-06-06 08:33:20 -0300

    FInalização

--- Atividade ToDoList - 30-05-2025/script.js
+++ Atividade ToDoList - 30-05-2025/script.js
@@ -1,15 +1,37 @@
 let botao = document.getElementById("add");
+let lista = document.querySelector("#lista");
 
 botao.addEventListener("click", function (evt) {
   evt.preventDefault();
-  console.log("Foi");
   let campo = document.querySelector("input");
-  let lista = document.querySelector("#lista");
   let item = document.createElement("li");
+
+  item.innerHTML = `
+    <input type='checkbox' class='checkbox'>
+    <span class='texto'>${campo.value}</span>
+    <span class='remover'>Delete</span>
+    <span class='editar'>Edit</span>
+  `;
+
   lista.appendChild(item);
-  item.innerHTML =
-    "<span class='texto'>" +
-    campo.value +
-    "</span> <span class='remover'>Delete</span> <span class='editar'>Edit</span>";
-  console.log(item);
+  campo.value = "";
+});
+
+lista.addEventListener("click", function (evt) {
+  let elemento = evt.target;
+
+  if (elemento.className === "remover") {
+    elemento.parentElement.remove();
+  } else if (elemento.className === "editar") {
+    const texto = elemento.parentElement.querySelector(".texto");
+    texto.contentEditable = true;
+    texto.focus();
+  } else if (elemento.className === "checkbox") {
+    const texto = elemento.parentElement.querySelector(".texto");
+    if (elemento.checked) {
+      texto.classList.add("riscado");
+    } else {
+      texto.classList.remove("riscado");
+    }
+  }
 });
